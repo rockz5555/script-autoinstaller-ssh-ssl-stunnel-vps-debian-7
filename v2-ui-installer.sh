216965e30a2426e6660531a20c1d98157b7ccdfc
Nizwer <nizwer.therock@gmail.com>
2021-03-11 21:09:04 +0530

    v2-ui installer script added

---
+++ v2-ui-installer.sh
@@ -0,0 +1,256 @@
+#!/usr/bin/env bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+cur_dir=$(pwd)
+
+echo "English by Nijoo"; echo;
+
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${red}Error：${plain} This script must be run as root user！\n" && exit 1
+
+# check os
+if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+else
+    echo -e "${red}The system version is not detected, please contact the script author！${plain}\n" && exit 1
+fi
+
+if [ $(getconf WORD_BIT) != '32' ] && [ $(getconf LONG_BIT) != '64' ] ; then
+    echo "This software does not support 32-bit system (x86), please use 64-bit system (x86_64), if the detection is wrong, please contact the author"
+    exit -1
+fi
+
+os_version=""
+
+# os version
+if [[ -f /etc/os-release ]]; then
+    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
+fi
+if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
+    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
+fi
+
+if [[ x"${release}" == x"centos" ]]; then
+    if [[ ${os_version} -le 6 ]]; then
+        echo -e "${red}Please use CentOS 7 or higher！${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"ubuntu" ]]; then
+    if [[ ${os_version} -lt 16 ]]; then
+        echo -e "${red}Please use Ubuntu 16 or higher！${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"debian" ]]; then
+    if [[ ${os_version} -lt 8 ]]; then
+        echo -e "${red}Please use Debian 8 or higher！${plain}\n" && exit 1
+    fi
+fi
+
+confirm() {
+    if [[ $# > 1 ]]; then
+        echo && read -p "$1 [default$2]: " temp
+        if [[ x"${temp}" == x"" ]]; then
+            temp=$2
+        fi
+    else
+        read -p "$1 [y/n]: " temp
+    fi
+    if [[ x"${temp}" == x"y" || x"${temp}" == x"Y" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+install_base() {
+    if [[ x"${release}" == x"centos" ]]; then
+        yum install wget curl tar unzip -y
+    else
+        apt install wget curl tar unzip -y
+    fi
+}
+
+uninstall_old_v2ray() {
+    if [[ -f /usr/bin/v2ray/v2ray ]]; then
+        confirm "The old version of v2ray is detected, wanna uninstall it? /usr/bin/v2ray/ VS /etc/systemd/system/v2ray.service" "Y"
+        if [[ $? != 0 ]]; then
+            echo "Cannot install v2-ui without uninstalling"
+            exit 1
+        fi
+        echo -e "${green}Uninstall the old version of v2ray${plain}"
+        systemctl stop v2ray
+        rm /usr/bin/v2ray/ -rf
+        rm /etc/systemd/system/v2ray.service -f
+        systemctl daemon-reload
+    fi
+    if [[ -f /usr/local/bin/v2ray ]]; then
+        confirm "V2ray installation detected, wanna uninstall it? v2-ui comes with the official v2ray kernel, in order to prevent conflicts with its ports, it is recommended to uninstall" "Y"
+        if [[ $? != 0 ]]; then
+            echo -e "${red}You choose not to uninstall, please ensure that the v2ray installation and the official v2ray kernel ${red} that comes with v2-ui ${green} will not conflict with ports${plain}"
+        else
+            echo -e "${green}Start uninstalling other V2ray installations ${plain}"
+            systemctl stop v2ray
+            bash <(curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --remove
+            systemctl daemon-reload
+        fi
+    fi
+}
+
+install_v2ray() {
+    uninstall_old_v2ray
+    echo -e "${green}Start to install or upgrade v2ray${plain}"
+    bash <(curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+    if [[ $? -ne 0 ]]; then
+        echo -e "${red}v2ray installation or upgrade failed, please check the error message${plain}"
+        echo -e "${yellow}Most of the reasons may be caused by the inability to download the v2ray installation package in the area where your current server is located. This is more common on domestic machines. The solution is to manually install v2ray. For the specific reason, please see the error message above.${plain}"
+        exit 1
+    fi
+    echo "
+[Unit]
+Description=V2Ray Service
+After=network.target nss-lookup.target
+
+[Service]
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+Environment=V2RAY_LOCATION_ASSET=/usr/local/share/v2ray/
+ExecStart=/usr/local/bin/v2ray -confdir /usr/local/etc/v2ray/
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/v2ray.service
+    if [[ ! -f /usr/local/etc/v2ray/00_log.json ]]; then
+        echo "{}" > /usr/local/etc/v2ray/00_log.json
+    fi
+    if [[ ! -f /usr/local/etc/v2ray/01_api.json ]]; then
+        echo "{}" > /usr/local/etc/v2ray/01_api.json
+    fi
+    if [[ ! -f /usr/local/etc/v2ray/02_dns.json ]]; then
+        echo "{}" > /usr/local/etc/v2ray/02_dns.json
+    fi
+    if [[ ! -f /usr/local/etc/v2ray/03_routing.json ]]; then
+        echo "{}" > /usr/local/etc/v2ray/03_routing.json
+    fi
+    if [[ ! -f /usr/local/etc/v2ray/04_policy.json ]]; then
+        echo "{}" > /usr/local/etc/v2ray/04_policy.json
+    fi
+    if [[ ! -f /usr/local/etc/v2ray/05_inbounds.json ]]; then
+        echo "{}" > /usr/local/etc/v2ray/05_inbounds.json
+    fi
+    if [[ ! -f /usr/local/etc/v2ray/06_outbounds.json ]]; then
+        echo "{}" > /usr/local/etc/v2ray/06_outbounds.json
+    fi
+    if [[ ! -f /usr/local/etc/v2ray/07_transport.json ]]; then
+        echo "{}" > /usr/local/etc/v2ray/07_transport.json
+    fi
+    if [[ ! -f /usr/local/etc/v2ray/08_stats.json ]]; then
+        echo "{}" > /usr/local/etc/v2ray/08_stats.json
+    fi
+    if [[ ! -f /usr/local/etc/v2ray/09_reverse.json ]]; then
+        echo "{}" > /usr/local/etc/v2ray/09_reverse.json
+    fi
+    systemctl daemon-reload
+    systemctl enable v2ray
+    systemctl start v2ray
+}
+
+#close_firewall() {
+#    if [[ x"${release}" == x"centos" ]]; then
+#        systemctl stop firewalld
+#        systemctl disable firewalld
+#    elif [[ x"${release}" == x"ubuntu" ]]; then
+#        ufw disable
+#    elif [[ x"${release}" == x"debian" ]]; then
+#        iptables -P INPUT ACCEPT
+#        iptables -P OUTPUT ACCEPT
+#        iptables -P FORWARD ACCEPT
+#        iptables -F
+#    fi
+#}
+
+install_v2-ui() {
+    systemctl stop v2-ui
+    cd /usr/local/
+    if [[ -e /usr/local/v2-ui/ ]]; then
+        rm /usr/local/v2-ui/ -rf
+    fi
+
+    if  [ $# == 0 ] ;then
+        last_version=$(curl -Ls "https://api.github.com/repos/sprov065/v2-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        if [[ ! -n "$last_version" ]]; then
+            echo -e "${red}Failed to detect the v2-ui version, it may be beyond the Github API limit, please try again later, or manually specify the v2-ui version to install${plain}"
+            exit 1
+        fi
+        echo -e "The latest version of v2-ui detected：${last_version}, start installation"
+        wget -N --no-check-certificate -O /usr/local/v2-ui-linux.tar.gz https://github.com/sprov065/v2-ui/releases/download/${last_version}/v2-ui-linux.tar.gz
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}Download v2-ui failed, please make sure your server can download Github files${plain}"
+            exit 1
+        fi
+    else
+        last_version=$1
+        url="https://github.com/sprov065/v2-ui/releases/download/${last_version}/v2-ui-linux.tar.gz"
+        echo -e "Start installation v2-ui v$1"
+        wget -N --no-check-certificate -O /usr/local/v2-ui-linux.tar.gz ${url}
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}Download v2-ui v$1 failed, please make sure this version exists${plain}"
+            exit 1
+        fi
+    fi
+
+    tar zxvf v2-ui-linux.tar.gz
+    rm v2-ui-linux.tar.gz -f
+    cd v2-ui
+    chmod +x v2-ui bin/v2ray-v2-ui bin/v2ctl
+    cp -f v2-ui.service /etc/systemd/system/
+    systemctl daemon-reload
+    systemctl enable v2-ui
+    systemctl start v2-ui
+    echo -e "${green}v2-ui v${last_version}${plain} The installation is complete and the panel has been activated，"
+    echo -e ""
+    echo -e "If it is a new installation, the default web port is ${green}65432${plain}, The username and password are both by default: ${green}admin${plain}"
+    echo -e "Please make sure that this port is not occupied by other programs. ${yellow}And make sure that port 65432 has been released${plain}"
+    echo -e "If you want to modify 65432 to another port, enter the v2-ui command to modify it, and also make sure that the port you modify is also allowed"
+    echo -e ""
+    echo -e "If it is to update the panel, access the panel as you did before"
+    echo -e ""
+    curl -o /usr/bin/v2-ui -Ls https://raw.githubusercontent.com/sprov065/v2-ui/master/v2-ui.sh
+    chmod +x /usr/bin/v2-ui
+    echo -e "v2-ui How to use: "
+    echo -e "----------------------------------------------"
+    echo -e "v2-ui              - Display management menu (more functions)"
+    echo -e "v2-ui start        - Start the v2-ui panel"
+    echo -e "v2-ui stop         - Stop v2-ui panel"
+    echo -e "v2-ui restart      - Restart v2-ui panel"
+    echo -e "v2-ui status       - View v2-ui status"
+    echo -e "v2-ui enable       - Set v2-ui to start automatically after booting"
+    echo -e "v2-ui disable      - Cancel v2-ui boot automatically"
+    echo -e "v2-ui log          - View v2-ui log"
+    echo -e "v2-ui update       - Update v2-ui panel"
+    echo -e "v2-ui install      - Install v2-ui panel"
+    echo -e "v2-ui uninstall    - Uninstall v2-ui panel"
+    echo -e "----------------------------------------------"
+}
+
+echo -e "${green}Start installation${plain}"
+install_base
+uninstall_old_v2ray
+#close_firewall
+install_v2-ui $1
